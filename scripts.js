45e23646ec142c8879ae0d2b0db6b07972ed5e77
Cherno Valdor <y.labchiri.2021@alumnos.urjc.es>
2023-10-15 11:21:33 +0200

    New Changes

--- scripts.js
+++ scripts.js
@@ -2,16 +2,20 @@
 var teams = new Map()
 
 class Team {
-    constructor(name, descr, img, date) {
+    constructor(name, descr, img, date, soccers) {
         this.name = name
         this.descr = descr
         this.img = img
         this.date = date
-        this.soccers = new Map()
+        if (soccers === null || soccers === undefined){
+            this.soccers = []
+        }else{
+            this.soccers = soccers
+        }
     }
 }
 
-class Soccers {
+class Soccer {
     constructor(name,age,img){
         this.name = name
         this.age = age
@@ -19,9 +23,18 @@ class Soccers {
     }
 }
 
+let soccers1 = []
+let soccer1 = new Soccer("Arion",15,"soccers_img/descarga.jpeg")
+let soccer2 = new Soccer("Arion",15,"soccers_img/descarga.jpeg")
+let soccer3 = new Soccer("Arion",15,"soccers_img/descarga.jpeg")
+let soccer4 = new Soccer("Arion",15,"soccers_img/descarga.jpeg")
+let soccer5 = new Soccer("Arion",15,"soccers_img/descarga.jpeg")
+let soccer6 = new Soccer("Arion",15,"soccers_img/descarga.jpeg")
+soccers1.push(soccer1,soccer2,soccer3,soccer4,soccer5,soccer6)
+
 let team1 = new Team("Dark Knights","The most powerfull Knights!","img/images.jpeg","27/08/1999");
 let team2 = new Team("Raimon","Old, but still good","img/raimon.jpg","18/07/1985");
-let team3 = new Team("New Raimon","The new Version!","img/newraimon.png","27/08/2008");
+let team3 = new Team("New Raimon","The new Version!","img/newraimon.png","27/08/2008",soccers1);
 
 teams.set(team1.name,team1)
 teams.set(team2.name,team2)
@@ -40,6 +53,10 @@ function showHide(element) {
     } else {
         elm.style.display = "none"
     }
+}
+function goBackToTeams(){
+    displayMoreInfoTeam()
+    document.getElementById("main_page").style = ""
 }
 function displayCreateForm() {
     if (document.getElementById("form_Element").style.display === "none") {
@@ -60,10 +77,8 @@ function createTeam() {
     addTeam(new_team)
     displayCreateForm()
 }
-
 function addTeam(eq) {
     teams.set(eq.name,eq)
-    let img
     addToDomTeam(eq)
 }
 function addToDomTeam(eq){
@@ -73,19 +88,52 @@ function addToDomTeam(eq){
             <div class="small text-muted">${eq.date}</div>
             <h2 class="card-title h4">${eq.name}</h2>
             <p class="card-text">${eq.descr}</p>
-            <button class="btn btn-primary" onclick="more_info("${eq.name}")">Read more →</button>
+            <button class="btn btn-primary" onclick="more_info('${eq.name}')">Read more →</button>
         </div>
     </div>`
 }
-function displayMoreInfoTeam(eq){
+function displayMoreInfoTeam(){
+    showHide("sub_zone")
     showHide("main_page")
-    showHide("team_info")
-    
+}
+function getAllHtmlSoccers(soccers){
+    let text = ""
+    if (soccers.length > 0){soccers.forEach(element => {
+        text += `<div class="col mb-5">
+        <div class="card h-100">
+            <!-- Product image-->
+            <img class="card-img-top" src="${element.img}"/>
+            <!-- Product details-->
+            <div class="card-body p-4">
+                <div class="text-center">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Name : ${element.name}</li>
+                        <li class="list-group-item">Age : ${element.age}</li>
+                    </ul>
+                </div>
+            </div>
+            <!-- Product actions-->
+            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                <div class="text-center"><a class="btn btn-outline-dark mt-auto">Modify info</a></div>
+            </div>
+        </div>
+    </div>`
+    });}else{
+        text = `<div class="alert alert-danger alertNoSoccers" role="alert">
+        <h4 class="alert-heading">Well done!</h4>
+        <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+        <hr>
+        <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+      </div>`
+    }
+    return text
 }
 function more_info(eq_name){
     if (! teams.has(eq_name)){
         alert("This team doesn't exist")
     }else{
-        displayMoreInfoTeam(teams.get(eq_name))
+        displayMoreInfoTeam()
+        txt = getAllHtmlSoccers(teams.get(eq_name).soccers)
+        document.getElementById("soccers").innerHTML = txt
     }
 } 
